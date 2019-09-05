da76b9fb6e7f4ac39daa65b7525c6f4c455fabd5
Dilpesh Patel <robotjandal@gmail.com>
2019-09-05 15:03:09 +1200

    Introducing gtest
    Gtest has been integrated as a cmake dependency (recommended method).
    Moved finding of other cmake projects to the root CMakeLists.txt file to be visible to tests.
    Using a very basic test to ensure a test can be run.

---
+++ test/parse_yaml_test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+namespace ImageProcessor {
+namespace {
+// The fixture for testing class Foo.
+
+TEST(hello_world_test, NoArguments) {
+  EXPECT_EQ("Hello, World!", "Hello, World!");
+}
+
+} // namespace
+} // namespace ImageProcessor
